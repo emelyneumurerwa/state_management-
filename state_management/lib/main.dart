734dd6c3ad6534a3c32a422aab7ddff0ccb5e1f7
Emelyne <emelyneumurerwa28@gmail.com>
2026-02-28 18:28:38 +0200

    first commit

---
+++ state_management/lib/main.dart
@@ -0,0 +1,37 @@
+ import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'counter_model.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterModel(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Provider Example',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Provider Example'),
+      ),
+      body: Center(
+        child: Text('State is being provided!'),
+      ),
+    );
+  }
+}
